81f21921384b8576261cc0ca63ad341b24d19874
RealLinen <94056347+RealLinen@users.noreply.github.com>
2022-10-09 14:22:03 -0400

    r

---
+++ Frontend/main.lua
@@ -0,0 +1,30 @@
+local API = {}
+local WS;
+if typeof(syn)=="table" then WS = syn.websocket end
+if WebSocket or Websocket then WS = ( WebSocket or Websocket ) end -- ScriptWare or KRNL and etc...
+if not WS or not WS["connect"] then return warn("Cannot Load Universal Chat, Exploit doesn't support Websockets! [ Or ur using SynX V3, Fixing that later ]"); end
+--------------------------------------------
+function API:LoadChat(UI: Frame, Layout: TextButton)
+    if typeof(UI)~="Instance" then return "The first argument must be a Frame where with a UIGridLayout were a button of the people who text can be created can be showed or put inside." end
+    if typeof(Layout)~="Instance" or not Layout:IsA("TextButton") then 
+        Layout = "" -- button
+    end
+    local connection = WS.connect("ws://localhost:8000/")
+    connection.OnMessage:Connect(function(msg)
+        if type(msg)=="table" then
+            local msgtype = msg["type"]
+            local usrData = msg["userdata"]
+            if msgtype=="__NEWMESSAGE__" and type(usrData)=="table" then
+                local cl = Layout:Clone()
+                local D_Icon: ImageButton = cl:FindFirstChild("Icon")
+                local D_Username: TextLabel = cl:FindFirstChild("Username")
+                local D_Message: TextLabel = cl:FindFirstChild("Message")
+                if D_Icon then pcall(function() D_Icon.Image = usrData["avatarURLIMAGE"] end) end
+                if D_Username then pcall(function() D_Username.Text = (usrData["username"] or "<no username>") end) end
+                if D_Message then pcall(function() D_Message.Text = (usrData["message"] or "<no message>") end) end
+                cl.Parent = UI
+            end
+        end
+    end)
+end
+--------------------------------------------
