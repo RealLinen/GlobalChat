18fab13e634d0e7d90ed87d3c74930fbae697967
RealLinen <94056347+RealLinen@users.noreply.github.com>
2022-10-09 15:23:20 -0400

    Update main.lua

--- Frontend/main.lua
+++ Frontend/main.lua
@@ -6,7 +6,7 @@ if not WS or not WS["connect"] then return warn("Cannot Load Universal Chat, Exp
 --------------------------------------------
 function API:LoadChat(UI: Frame, Layout: TextButton)
     if typeof(UI)~="Instance" then return "The first argument must be a Frame where with a UIGridLayout were a button of the people who text can be created can be showed or put inside." end
-    if typeof(Layout)~="Instance" or not Layout:IsA("TextButton") then 
+    if typeof(Layout)~="Instance" or (not Layout:IsA("TextButton") or not Layout:IsA("ScrollingFrame") or not Layout:IsA("Frame")) then 
         Layout = "" -- button
     end
     local connection = WS.connect("ws://localhost:8000/")
@@ -22,7 +22,18 @@ function API:LoadChat(UI: Frame, Layout: TextButton)
                 if D_Icon then pcall(function() D_Icon.Image = usrData["avatarURLIMAGE"] end) end
                 if D_Username then pcall(function() D_Username.Text = (usrData["username"] or "<no username>") end) end
                 if D_Message then pcall(function() D_Message.Text = (usrData["message"] or "<no message>") end) end
+                cl.Visible = true
+                cl.Name = tostring(usrData["username"])..":"..tostring(usrData["message"])
                 cl.Parent = UI
+            end
+            if msgtype=="__DELETEMESSAGE__" and type(usrData)=="table" then
+                local msgName = usrData["msgName"]
+                if type(msgName)=="string" then
+                    local found = UI:FindFirstChild(msgName)
+                    if found then
+                        found:Destroy()
+                    end
+                end
             end
         end
     end)
